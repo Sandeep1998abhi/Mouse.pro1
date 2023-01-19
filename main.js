6a050446221fb3340624b0068e3e97a3483c04ce
sandeep <abhisandeep@1998>
2023-01-19 17:54:30 +0530

    project commit

---
+++ main.js
@@ -0,0 +1,48 @@
+
+const swiper = new Swiper(".gallerySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+
+  },
+  breakpoints: {
+    800: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
+});
+
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu-btn');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu-open');
+});
+
+const pagination = document.querySelectorAll('.pagination li a');
+const sections = document.querySelectorAll('.main-section');
+const offset = 100;
+
+for (let i = 0; i < sections.length; i++) {
+
+  //add scroll
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > sections[i].offsetTop - offset &&
+        window.scrollY < sections[i].offsetTop + sections[i].offsetHeight - offset) {
+
+
+      pagination.forEach(pag => {
+        pag.classList.remove('active');
+
+      });
+      pagination[i].classList.add('active');
+    }
+  });
+}
